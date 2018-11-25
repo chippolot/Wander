ae8b3eb10baa16606136e55596e73fec6036f9a4
Ben <ben@plutovr.com>
2018-11-25 15:40:52 -0800

    First commit

---
+++ Wander/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wander
+//
+//  Created by Ben on 11/25/18.
+//  Copyright © 2018 Ben. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
