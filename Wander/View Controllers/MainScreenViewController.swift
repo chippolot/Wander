92eea77e4799072baa2fac3c7b8273006ca54bf2
Ben <ben@plutovr.com>
2018-11-25 21:48:42 -0800

    Cleanup

--- Wander/View Controllers/MainScreenViewController.swift
+++ Wander/View Controllers/MainScreenViewController.swift
@@ -10,7 +10,6 @@ import UIKit
 import MapKit
 import RxSwift
 
-//TODO: Should initialize both LocationManager AND MKMapView here?
 //TODO: Setup core data
 //TODO: Show wander info on screen
 class MainScreenViewController: UIViewController {
@@ -48,16 +47,6 @@ class MainScreenViewController: UIViewController {
     deinit {
         locationManager.stopUpdatingLocation()
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 }
 
 extension MainScreenViewController: MKMapViewDelegate {
